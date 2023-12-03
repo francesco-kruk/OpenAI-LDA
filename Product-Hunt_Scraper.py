1a8a1b75b44697f442df8aa9282374cf0a407d43
francesco-kruk <keziahjohnson97@gmail.com>
2023-12-03 14:18:25 +0100

    Updated docs

--- Product-Hunt_Scraper.py
+++ Product-Hunt_Scraper.py
@@ -1,39 +1,51 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+import time
 
-review = {}
+driver = webdriver.Chrome()
+driver.get("https://www.producthunt.com/products/openai/reviews")
+
+while True:
+    try:
+        button = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            (By.XPATH, "//button[@class='styles_reset__1_PU9 styles_button__7X8Df styles_full__wgSm4 mb-8']")))
+        # button = driver.find_element(By.XPATH,"//button[@class=
+        #                                       'styles_reset__1_PU9 styles_button__7X8Df styles_full__wgSm4 mb-8']")
+        button.click()
+    except:
+        print("Page loaded!")
+        break
+
+print("Progressing")
+page_source = driver.page_source
 review_list = []
-count = 0
+fields = ["Review text"]
 
-response = requests.get(f"https://www.producthunt.com/products/openai/reviews")
-web_page = response.text
+# response = requests.get("https://www.producthunt.com/products/openai/reviews")
+# web_page = response.text
 
-"""
-To add more elements per page could use Postman/Insomnia as shown in https://www.youtube.com/watch?v=DqtlR0y0suo
-"""
+soup = BeautifulSoup(page_source, "html.parser")
+target = soup.find("div", {"class": "flex direction-column flex-column-gap-8 mt-6 mb-default"})
+review_elements = target.findChildren("div", recursive=False)
 
-if response.status_code == 200:
-    soup = BeautifulSoup(web_page, "html.parser")
-    target = soup.find("div", {"class": "flex direction-column flex-column-gap-8 mt-6 mb-default"})
-    review_elements = target.findChildren("div", recursive=False)
-    print(review_elements)
-
-    for review_element in review_elements:
-        review["review_name"] = review_element.find("a", {"class": "color-darker-grey fontSize-16 fontWeight-600"}).text
-        # review["review_score"] = review_element.find("div", {"class": "flex direction-row"}).find('img').get('alt')
-        # Score could be implemented by going through the stars and counting how many filled in
-        if review_element.find("div", {"class": "styles_htmlText__iftLe"}) == None:
-            review_list.append(review)
-            review = {}
-            continue
-        else:
-            review["review_text"] = review_element.find("div", {"class": "styles_htmlText__iftLe"}).text
+for review_element in review_elements:
+    review = []
+    if review_element.find("div", {"class": "styles_htmlText__iftLe"}) == None:
         review_list.append(review)
-        review = {}
-        count = count + 1
-        print(count)
+        continue
+    else:
+        review.append(review_element.find("div", {"class": "styles_htmlText__iftLe"}).text)
+    review_list.append(review)
 
-else:
-    print("Failed to retrieve the page. Status code:", response.status_code)
-
-print(review_list)
+with open('Product_Hunt.csv', 'w') as f:
+    write = csv.writer(f)
+    write.writerow(fields)
+    write.writerows(review_list)
--- Trustpilot_Scraper.py
+++ Trustpilot_Scraper.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 from_page = 1
 to_page = 6
-review={}
-review_list=[]
+review_list = []
+fields = ["Review score (out of 5)", "Review text"]
 
 for i in range(from_page, to_page + 1):
     response = requests.get(f"https://www.trustpilot.com/review/openai.com?page={i}")
@@ -15,19 +16,23 @@ for i in range(from_page, to_page + 1):
         review_elements = soup.find_all("div", {"class": "styles_cardWrapper__LcCPA"})
 
         for review_element in review_elements:
-            review["review_name"] = review_element.find("span", {"class": "typography_heading-xxs__QKBS8"}).text
-            review["review_score"] = review_element.find("div", {"class": "star-rating_starRating__4rrcf"}).find('img').get('alt')
-            review["review_title"] = review_element.find("h2", {"class": "typography_heading-s__f7029"}).text
+            review = []
+            #review.append(review_element.find("span", {"class": "typography_heading-xxs__QKBS8"}).text)
+            score = review_element.find("div", {"class": "star-rating_starRating__4rrcf"}).find('img').get('alt')
+            score_num = score[6]
+            review.append(score_num)
+            #review.append(review_element.find("h2", {"class": "typography_heading-s__f7029"}).text)
             if review_element.find("p", {"class": "typography_body-l__KUYFJ"}) == None:
                 review_list.append(review)
-                review = {}
                 continue
             else:
-                review["review_text"] = review_element.find("p", {"class": "typography_body-l__KUYFJ"}).text
+                review.append(review_element.find("p", {"class": "typography_body-l__KUYFJ"}).text)
             review_list.append(review)
-            review = {}
 
     else:
         print("Failed to retrieve the page. Status code:", response.status_code)
 
-print(review_list)
+with open('Trustpilot.csv', 'w') as f:
+    write = csv.writer(f)
+    write.writerow(fields)
+    write.writerows(review_list)
